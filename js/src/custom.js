3edf423d30b50b7c5808e104ff184290810d5f22
LeFlacon <15356373111@163.com>
2018-08-26 02:54:30 +0800

    Site updated: 2018-08-26 02:54:29

---
+++ js/src/custom.js
@@ -0,0 +1,12 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([  
+          "/images/background/1.jpg",
+          "/images/background/2.jpg",
+          "/images/background/3.jpg",
+          "/images/background/4.jpg",
+          "/images/background/5.jpg",
+          "/images/background/6.jpg",
+          "/images/background/7.jpg"
+    ], { duration: 60000, fade: 1500 });  
+});
